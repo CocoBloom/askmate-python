2785406b7f352597e5668372ec71a9fce8e0a143
Gábor Hajdu <gabor.hajdu@codecool.com>
2020-06-26 13:07:45 +0200

    add registration endpoint

--- server.py
+++ server.py
@@ -385,6 +385,11 @@ def accept_answer(answer_id):
     return redirect('/question/' + str(question_id))
 
 
+@app.route('/registration_2')
+def registration_2():
+    pass
+
+
 if __name__ == "__main__":
     app.run(debug=True,
             port=8000,
